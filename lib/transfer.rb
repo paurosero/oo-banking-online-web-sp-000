49aaafce83915d1d8d52ec569b8ddbae32aef19c
Paul Rosero <paul_rosero@yahoo.com>
2019-05-19 00:08:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,9 @@ class Transfer
   end
   
   def reverse_transfer
-    
+    valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed
   end
 end
