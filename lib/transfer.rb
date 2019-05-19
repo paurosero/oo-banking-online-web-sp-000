942ebedb5c14980f9e02ad16d6d9efd63ba4e9e3
Paul Rosero <paul_rosero@yahoo.com>
2019-05-19 00:03:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,13 @@ class Transfer
   end
   
   def execute_transaction
-    
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
   end
   
   def reverse_transfer
